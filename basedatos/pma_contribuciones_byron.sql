1995a70c81eca7db41188690a553f5aeb7a59a89
byron <byronherrera@hotmail.com>
2019-07-01 18:16:20 -0500

    byron

--- basedatos/pma_contribuciones_byron.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `pma-desarrollo`.`pma_contribuciones` 
-CHANGE COLUMN `grant_TOD` `grant_tod` date NULL DEFAULT NULL AFTER `total_grant`,
-CHANGE COLUMN `grant_TDD` `grant_tdd` date NULL DEFAULT NULL AFTER `grant_tod`;
-
-DELETE FROM `pma-desarrollo`.`qo_privileges` WHERE `id` > 4;
--- basedatos/pma_so_categories_201906242004.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO pma_desarrollo.pma_so_categories (id__cost_category,category_name,category_code) VALUES 
-(1,'SO1','EC01.01.011')
-,(2,'SO2','EC01.03.021')
-,(3,'SO3','EC01.04.031')
-,(4,'SO4','EC01.05.041 ')
-;
